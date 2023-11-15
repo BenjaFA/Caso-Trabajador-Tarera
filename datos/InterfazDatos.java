a13735aa18436752b378413a6cd84e837e6131f9
BenjaFA <b.fernandez07@ufromail.cl>
2023-11-15 09:29:50 -0300

    Code generated by UML

---
+++ datos/InterfazDatos.java
@@ -0,0 +1,11 @@
+package datos;
+
+public interface InterfazDatos {
+
+	void read();
+
+	void open();
+
+	void close();
+
+}
---
+++ ventanas2/GuiTrabajador.java
@@ -0,0 +1,10 @@
+package ventanas2;
+
+import java.*;
+
+public class GuiTrabajador extends JFrame implements ActionListener {
+
+	private JButton btnLectura;
+
+
+}
